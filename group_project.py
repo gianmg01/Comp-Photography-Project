1846653ef9b2b0b24700d4bb9e4084e83c062cc4
root <root@LAPTOP-K1N2SKVM.localdomain>
2023-12-08 18:17:18 -0500

    made some changes

--- group_project.py
+++ group_project.py
@@ -8,6 +8,28 @@ import numpy as np
 import cv2
 from easyocr import Reader
 
+def findAndRemoveWord(image, word):
+    img_copy = np.copy(image)
+    langs = ["en"]
+    reader = Reader(langs)
+    results = reader.readtext(image, width_ths=0)
+
+    for (bbox, text, prob) in results:
+        if text.lower() == word.lower():
+            (top_left, top_right, bottom_right, bottom_left) = bbox
+            tl = (int(top_left[0]), int(top_left[1]))
+            br = (int(bottom_right[0]), int(bottom_right[1]))
+            
+            cv2.rectangle(img_copy, tl, br, (255, 255, 255), -1)  # Fill the region with white
+
+    return img_copy
+
+def resizeImage(image, scale_factor):
+    h, w, _ = image.shape
+    new_h, new_w = int(h * scale_factor), int(w * scale_factor)
+    resized_image = cv2.resize(image, (new_w, new_h))
+    return resized_image
+
 def getText(image, show_boxes=True):
     langs = ["en"]
     reader = Reader(langs)
@@ -40,7 +62,7 @@ def transformImg(img, add_boxes=True):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     w = img.shape[1]
     h = img.shape[0]
-    min_val = 255
+    min_val = 900
     for i in range(0, h):
         for j in range(0, w):
             if img_gray[i, j] < min_val:
@@ -58,27 +80,16 @@ def transformImg(img, add_boxes=True):
     
     return img
 
-def findAndRemoveWord(image, word):
-    img_copy = np.copy(image)
-    langs = ["en"]
-    reader = Reader(langs)
-    results = reader.readtext(image, width_ths=0)
+def gaussianBlur(img, kernel_size=(5, 5), sigmaX=0):
+    blurred_img = cv2.GaussianBlur(img, kernel_size, sigmaX)
+    return blurred_img
 
-    for (bbox, text, prob) in results:
-        if text.lower() == word.lower():
-            (top_left, top_right, bottom_right, bottom_left) = bbox
-            tl = (int(top_left[0]), int(top_left[1]))
-            br = (int(bottom_right[0]), int(bottom_right[1]))
-            
-            cv2.rectangle(img_copy, tl, br, (255, 255, 255), -1)  # Fill the region with white
-
-    return img_copy
-
-def resizeImage(image, scale_factor):
-    h, w, _ = image.shape
-    new_h, new_w = int(h * scale_factor), int(w * scale_factor)
-    resized_image = cv2.resize(image, (new_w, new_h))
-    return resized_image
+def normalizeBrightness(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    value = 0.5
+    hsv[:,:,2] = hsv[:,:,2] * value
+    normalized_img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    return normalized_img
 
 def main():
     layout = [
@@ -111,39 +122,44 @@ def main():
             if save_path:
                 save_image(save_path, image_data)
         elif event == "Clean and Get Text" and image_data is not None:
-            # Ensure that boxes are not added
-            image_data = transformImg(image_data, add_boxes=False)
-            text_result = getText(image_data, False)
-            update_image(window, image_data)
+            # Normalize brightness before transforming the image
+            normalized_data = normalizeBrightness(image_data)
+            update_image(window, normalized_data)
+            # Transform the image before extracting text
+            transformed_data = transformImg(normalized_data, add_boxes=False)
+            blurred_data = gaussianBlur(transformed_data)
+            update_image(window, blurred_data)
+            text_result = getText(transformed_data, False)
             window["-TEXTBOX-"].update(value=text_result)
         elif event == "Show Boxes" and image_data is not None:
             show_boxes = True
             # Draw boxes around words
-            img_with_boxes = np.copy(image_data)
+            img_with_boxes = np.copy(transformed_data)
             getText(img_with_boxes, show_boxes)
             update_image(window, img_with_boxes)
         elif event == "Hide Boxes" and image_data is not None:
             show_boxes = False
             # Remove boxes from the image
-            img_without_boxes = np.copy(image_data)
+            img_without_boxes = np.copy(transformed_data)
             getText(img_without_boxes, show_boxes)
             update_image(window, img_without_boxes)
         elif event == "Find and Remove" and image_data is not None:
             word_to_remove = values["-WORD-"]
             if word_to_remove.strip() != "":
-                image_data = findAndRemoveWord(image_data, word_to_remove)
-                update_image(window, image_data)
+                # Find and remove the specified word
+                transformed_data = findAndRemoveWord(transformed_data, word_to_remove)
+                update_image(window, transformed_data)
         elif event == "Increase Size" and image_data is not None:
             scale_factor += 1
-            image_data = resizeImage(image_data, scale_factor)
-            update_image(window, image_data)
+            transformed_data = resizeImage(transformed_data, scale_factor)
+            update_image(window, transformed_data)
         elif event == "Decrease Size" and image_data is not None:
             scale_factor = max(0.1, scale_factor - 0.1)
-            image_data = resizeImage(image_data, scale_factor)
-            update_image(window, image_data)
+            transformed_data = resizeImage(transformed_data, scale_factor)
+            update_image(window, transformed_data)
 
     window.close()
-    
+
 def load_image(image_path):
     image = cv2.imread(image_path)
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
